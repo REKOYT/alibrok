e4a8d3e1f38f1ff7ab7e055af23e2def783589fe
REKOYT <46628323+REKOYT@users.noreply.github.com>
2019-01-16 17:59:41 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("533565936788045824")
+setInterval(function() {
+channel.send(`REKOYT am gaming:stuck_out_tongue:`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
